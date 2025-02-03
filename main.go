e56ec25d0cc940e412ad3ffaa7b7be408f389122
Zahid <zahid-iqbal-alpha>
2025-02-03 12:43:00 +0500

    GC-180 removed env file

--- main.go
+++ main.go
@@ -4,19 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
-
 	"github.com/grapple-solution/grapple_cli/cmd"
-	"github.com/joho/godotenv"
 )
-
-func init() {
-	// Load .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-}
 
 func main() {
 	cmd.Execute()
