37e4d9af5c884b97322aaaecc6c295fbd2c17c91
Zahid <zahid-iqbal-alpha>
2025-05-07 10:42:16 +0500

    updated version file

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -6,9 +6,9 @@ package version
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
+	"github.com/grapple-solution/grapple_cli/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -26,34 +26,16 @@ var VersionCmd = &cobra.Command{
 
 // getVersion reads the version from the VERSION file
 func getVersion() string {
-	// Try to find VERSION file relative to executable
-	execPath, err := os.Executable()
-	if err == nil {
-		execDir := filepath.Dir(execPath)
-		rootDir := filepath.Dir(execDir)
-		versionPath := filepath.Join(rootDir, "VERSION")
 
+	versionPath, err := utils.GetResourcePath("VERSION")
+	if err == nil {
 		content, err := os.ReadFile(versionPath)
 		if err == nil {
 			return strings.TrimSpace(string(content))
 		}
 	}
 
-	// Fallback: try to find VERSION in current directory or parent directories
-	dir, err := os.Getwd()
-	if err == nil {
-		for i := 0; i < 3; i++ { // Try current dir and up to 2 parent dirs
-			versionPath := filepath.Join(dir, "VERSION")
-			content, err := os.ReadFile(versionPath)
-			if err == nil {
-				return strings.TrimSpace(string(content))
-			}
-			dir = filepath.Dir(dir)
-		}
-	}
-
-	// If all else fails, return a default version
-	return "development"
+	return versionPath
 }
 
 func init() {
